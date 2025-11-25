c1d333167f93dd944c60f19c55c5865821121bc8
moura92 <alisson_moura_rj@hotmail.com>
2025-11-25 11:25:32 -0300

    Projeto criado

---
+++ src/test/java/com/moura/calculoimc/CalculoimcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura.calculoimc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoimcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
