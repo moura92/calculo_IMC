808b70f37306128cd160202014114ff86b5b389d
moura92 <alisson_moura_rj@hotmail.com>
2025-12-11 15:36:46 -0300

    Verbos - POST, PUT, DELETE

--- src/main/java/com/moura/UsuarioServices.java
+++ src/main/java/com/moura/UsuarioServices.java
@@ -1,5 +1,7 @@
 package com.moura;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -7,29 +9,61 @@ import org.springframework.stereotype.Service;
 
 import com.moura.model.Usuario;
 
-
-
 @Service
 public class UsuarioServices {
 
 	private final AtomicLong idUsuario = new AtomicLong();
-	
+
 	private Logger logger = Logger.getLogger(UsuarioServices.class.getName());
 	/*
-	 * Um logger é uma ferramenta usada em aplicações Java (e praticamente toda linguagem) para registrar mensagens, como:
-		- erros
-		- avisos
-		- informações importantes
-		- passos de execução
-		- detalhes de requisições
-		- eventos inesperados
-		Logger é o jeito profissional de mostrar mensagens do sistema,
-		substituindo System.out.println, com níveis, filtros e arquivos de log.
+	 * Um logger é uma ferramenta usada em aplicações Java (e praticamente toda
+	 * linguagem) para registrar mensagens, como: - erros - avisos - informações
+	 * importantes - passos de execução - detalhes de requisições - eventos
+	 * inesperados Logger é o jeito profissional de mostrar mensagens do sistema,
+	 * substituindo System.out.println, com níveis, filtros e arquivos de log.
 	 */
-	
+
+	public List<Usuario> findAll() {
+		logger.info("Varios usuarios!");
+		List<Usuario> usuarios = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Usuario usuario = listaUsuario(i);
+			usuarios.add(usuario);
+		}
+
+		return usuarios;
+	}
+
+	private Usuario listaUsuario(int i) {
+		Usuario usuario = new Usuario();
+		usuario.setId(idUsuario.incrementAndGet());
+		usuario.setNome("Usuario - " + i);
+		usuario.setIdade(25);
+		usuario.setAltura(1.72);
+		usuario.setPeso(78.5);
+		double calculo = usuario.getPeso() / (usuario.getAltura() * usuario.getAltura());
+		double imc = Math.round(calculo * 100.0) / 100.0;
+		usuario.setImc(imc);
+
+		if (imc < 18.5) {
+			usuario.setClassificaçao("Magreza");
+		} else if (imc <= 24.9) {
+			usuario.setClassificaçao("Peso Normal");
+		} else if (imc <= 29.9) {
+			usuario.setClassificaçao("Sobrepeso");
+		} else if (imc <= 34.9) {
+			usuario.setClassificaçao("Obesidade Grau I");
+		} else if (imc <= 39.9) {
+			usuario.setClassificaçao("Obesidade Grau II");
+		} else if (imc >= 40) {
+			usuario.setClassificaçao("Obesiade Grau III - (mórbida)");
+		}
+		return usuario;
+	}
+
 	public Usuario findById(String id) {
 		logger.info("Encontrando um usuario!");
-				
+
 		Usuario usuario = new Usuario();
 		usuario.setId(idUsuario.incrementAndGet());
 		usuario.setNome("Arruda");
@@ -39,26 +73,34 @@ public class UsuarioServices {
 		double calculo = usuario.getPeso() / (usuario.getAltura() * usuario.getAltura());
 		double imc = Math.round(calculo * 100.0) / 100.0;
 		usuario.setImc(imc);
-		
-		if (imc < 18.5) {usuario.setClassificaçao("Magreza");
+
+		if (imc < 18.5) {
+			usuario.setClassificaçao("Magreza");
+		} else if (imc <= 24.9) {
+			usuario.setClassificaçao("Peso Normal");
+		} else if (imc <= 29.9) {
+			usuario.setClassificaçao("Sobrepeso");
+		} else if (imc <= 34.9) {
+			usuario.setClassificaçao("Obesidade Grau I");
+		} else if (imc <= 39.9) {
+			usuario.setClassificaçao("Obesidade Grau II");
+		} else if (imc >= 40) {
+			usuario.setClassificaçao("Obesiade Grau III - (mórbida)");
 		}
-		else if(imc <= 24.9) {usuario.setClassificaçao("Peso Normal");
-		}
-		else if(imc <= 29.9) {usuario.setClassificaçao("Sobrepeso");
-		}
-		else if(imc <= 34.9) {usuario.setClassificaçao("Obesidade Grau I");
-		}
-		else if(imc <= 39.9) {usuario.setClassificaçao("Obesidade Grau II");
-		}
-		else if(imc >= 40) {usuario.setClassificaçao("Obesiade Grau III - (mórbida)");
-		}
-		
 		return usuario;
-		
-		/*
-		 * 		
-		
-		 */
-	
+	}
+
+	public Usuario create(Usuario usuario) {
+		logger.info("Criando um usuario!");
+		return usuario;
+	}
+
+	public Usuario update(Usuario usuario) {
+		logger.info("Atualizando usuario!");
+		return usuario;
+	}
+
+	public void delete(String id) {
+		logger.info("Usuario deletado!");
 	}
 }
--- src/main/java/com/moura/controllers/UsuarioControllers.java
+++ src/main/java/com/moura/controllers/UsuarioControllers.java
@@ -1,70 +1,57 @@
 package com.moura.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.moura.UsuarioServices;
-import com.moura.exception.ParametroInvalidoException;
 import com.moura.model.Usuario;
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioControllers {
 
 	@Autowired
 	/*
-	 * 	OBS: com a anotation "@Autowired" o spring vai injetar a instancia da classe
-	 * 		 "UsuarioService" sem a necessidade de usar o "new UsuarioServices();"
-	 * 	desta forma: "private UsuarioServices usuarioService = new UsuarioServices();"
+	 * OBS: com a anotation "@Autowired" o spring vai injetar a instancia da classe
+	 * "UsuarioService" sem a necessidade de usar o "new UsuarioServices();" desta
+	 * forma: "private UsuarioServices usuarioService = new UsuarioServices();"
 	 */
 	private UsuarioServices usuarioService;
-	
-	private final AtomicLong id = new AtomicLong(); // Cria o ID
 
-	//http://localhost:8080/id
-	@RequestMapping("/{id}")
-	public Usuario teste(@PathVariable("id") String id) {
+	// http://localhost:8080/usuario
+	@RequestMapping()
+	public List<Usuario> findAll() {
+		return usuarioService.findAll();
+	}
+
+	// http://localhost:8080/1
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Usuario findById(@PathVariable("id") String id) {
 		return usuarioService.findById(id);
 	}
 
-	// http://localhost:8080/dados
-	@RequestMapping("/dados")
-	public Usuario dados(
-			@RequestParam(defaultValue = "Alisson de moura") String nome,
-			@RequestParam(defaultValue = "29") int idade,
-			@RequestParam(required = false) Double peso, 
-			@RequestParam(required = false) Double altura) {
+	// CREATE
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Usuario create(@RequestBody Usuario usuario) {
+		return usuarioService.create(usuario);
+	}
 
-		if (peso == null || peso <= 0) {
-			throw new ParametroInvalidoException("ERRO! O peso não deve estar vazia.");
-		}
+	// UPDATE
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Usuario update(@RequestBody Usuario usuario) {
+		return usuarioService.update(usuario);
+	}
 
-		if (altura == null || altura <= 0) {
-			throw new ParametroInvalidoException("ERRO! A altura não deve estar vazia.");
-		}
-		double calculo = peso / (altura * altura); // calculo IMC
-		double imc = Math.round(calculo * 100.0) / 100.0; // arredonda para duas casas decimais
-
-		String tabela = "";
-
-		if (imc < 18.5) {tabela = "Magreza";
-		}
-		else if(imc <= 24.9) {tabela = "Peso Normal";
-		}
-		else if(imc <= 29.9) {tabela = "Sobrepeso";
-		}
-		else if(imc <= 34.9) {tabela = "Obesidade Grau I";
-		}
-		else if(imc <= 39.9) {tabela = "Obesidade Grau II";
-		}
-		else if(imc >= 40) {tabela = "Obesiade Grau III - (mórbida)";
-		}
-		
-		
-		return new Usuario(id.incrementAndGet(), nome, idade, altura, peso, imc, tabela);
+	//DELETE
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") String id) {
+		usuarioService.delete(id);
 	}
 }
